ea5382e7f89b7e4cfb401071d2f70fc6af227e25
krishnadey30 <krishnakumardey.dey@gmail.com>
2020-06-20 05:49:07 +0530

    Adds tests for southeros

--- tests_all_unit.py
+++ tests_all_unit.py
@@ -2,12 +2,14 @@ import unittest
 from kingdom import Kingdom
 from Message import Message
 from MessageVerify import MessageVerify
+from Southeros import Southeros
+
 class TestKingdom(unittest.TestCase):
     def test_init(self):
         name = "ICE"
         emblem = "MAMMOTH"
         ice_kingdom = Kingdom(name, emblem)
-        self.assertNotEqual(None, ice_kingdom)
+        self.assertIsInstance(ice_kingdom, Kingdom)
         Kingdom.remove_all_kingdoms()
     
     def test_name(self):
@@ -100,6 +102,7 @@ class TestKingdom(unittest.TestCase):
         water_kingdom = Kingdom(name[2], emblem[2])
         fire_kingdom = Kingdom(name[3], emblem[3])
         ice_kingdom.add_ally(air_kingdom)
+        self.assertIsNone(Kingdom.get_ruler())
         ice_kingdom.add_ally(water_kingdom)
         ice_kingdom.add_ally(air_kingdom)
         ice_kingdom.add_ally(fire_kingdom)
@@ -108,6 +111,15 @@ class TestKingdom(unittest.TestCase):
         Kingdom.remove_all_kingdoms()
 
 class TestMessage(unittest.TestCase):
+    def test_init(self):
+        name = ["ICE", "AIR"]
+        emblem = ["MAMMOTH", "OWL"]
+        ice_kingdom = Kingdom(name[0], emblem[0])
+        air_kingdom = Kingdom(name[1], emblem[1])
+        message_text = "ROZO"
+        message = Message(air_kingdom, ice_kingdom, message_text)
+        self.assertIsInstance(message, Message)
+    
     def test_get_sender(self):
         name = ["ICE", "AIR"]
         emblem = ["MAMMOTH", "OWL"]
@@ -151,6 +163,63 @@ class TestMessageVerify(unittest.TestCase):
         message_text = "OWLAOWLBOWLC"
         response = MessageVerify.verify(air_kingdom, message_text)
         self.assertFalse(response)
+        Kingdom.remove_all_kingdoms()
+
+class TestSoutheros(unittest.TestCase):
+    def test_init(self):
+        southeros = Southeros()
+        self.assertIsInstance(southeros, Southeros)
+        Kingdom.remove_all_kingdoms()
+    
+    def test_register_kingdom(self):
+        southeros = Southeros()
+        southeros.register_kingdom("SPACE", "GORILLA")
+        self.assertEqual(1, southeros.get_total_kingdoms())
+        Kingdom.remove_all_kingdoms()
+    
+    def test_init_parameterised(self):
+        kingdoms = [("SPACE","GORILLA"), ("LAND","PANDA"),("WATER","OCTOPUS"),
+        ("ICE", "MAMMOTH"), ("AIR", "OWL"), ("FIRE", "DRAGON")]
+        southeros = Southeros(kingdoms)
+        self.assertIsInstance(southeros, Southeros)
+        Kingdom.remove_all_kingdoms()
+
+    def test_register_kingdoms(self):
+        kingdoms = [("SPACE","GORILLA"), ("LAND","PANDA"),("WATER","OCTOPUS"),
+        ("ICE", "MAMMOTH")]
+        southeros = Southeros(kingdoms)
+        self.assertEqual(4, southeros.get_total_kingdoms())
+        other_kingdoms = [("AIR", "OWL"), ("FIRE", "DRAGON")]
+        southeros.register_kingdoms(other_kingdoms)
+        self.assertEqual(6, southeros.get_total_kingdoms())
+        Kingdom.remove_all_kingdoms()
+    
+    def test_get_kingdom(self):
+        southeros = Southeros()
+        name = "SPACE"
+        emblem = "GORILLA"
+        southeros.register_kingdom(name, emblem)
+        space_kingdom = southeros.get_kingdom(name)
+        self.assertIsInstance(space_kingdom, Kingdom)
+        self.assertEqual(name, space_kingdom.name())
+        Kingdom.remove_all_kingdoms()
+
+    def test_ruler(self):
+        kingdoms = [("SPACE","GORILLA"), ("LAND","PANDA"),("WATER","OCTOPUS"),
+        ("ICE", "MAMMOTH")]
+        southeros = Southeros(kingdoms)
+        space_kingdom = southeros.get_kingdom("SPACE")
+        land_kingdom = southeros.get_kingdom("LAND")
+        water_kingdom = southeros.get_kingdom("WATER")
+        ice_kingdom = southeros.get_kingdom("ICE")
+        self.assertIsNone(southeros.ruler())
+        space_kingdom.add_ally(ice_kingdom)
+        space_kingdom.add_ally(land_kingdom)
+        space_kingdom.add_ally(water_kingdom)
+        ruler = southeros.ruler()
+        self.assertIsInstance(ruler, Kingdom)
+        self.assertEqual(ruler, space_kingdom)
+        self.assertNotEqual(ruler, ice_kingdom)
         Kingdom.remove_all_kingdoms()
 
 if __name__ == '__main__':
